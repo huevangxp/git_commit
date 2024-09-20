2f3026508a2c1b7d254bf5dfd66050ca543cf3a7
huevangxp <huevang933@gmail.comcd>
2024-09-20 14:24:10 +0700

    commit Fri 09/20/2024 14-24-10

--- git_commit.bat
+++ git_commit.bat
@@ -1,8 +1,23 @@
+
+
+
 @echo off
 :start
-set CURRENT_TIME=%DATE% %TIME%
+:: Format the date and time
+for /F "tokens=1-4 delims=/:. " %%a in ("%TIME%") do (
+    set HOUR=%%a
+    set MINUTE=%%b
+    set SECOND=%%c
+)
+set CURRENT_TIME=%DATE% %HOUR%-%MINUTE%-%SECOND%
+
+:: Git operations
 git add .
 git commit -m "commit %CURRENT_TIME%"
 git push
-timeout /t 2
+
+:: Wait for 20 seconds before repeating
+timeout /t 20
+
+:: Go back to start
 goto start

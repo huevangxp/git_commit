7707864a4b176bccd2c683ffcbcbd498a25df6ef
huevangxp <huevang933@gmail.comcd>
2024-09-20 14:39:18 +0700

    %datenow%

--- git_commit.bat
+++ git_commit.bat
@@ -1,19 +1,6 @@
 @echo off
-:start
-REM Get the current date and time
-set CURRENT_TIME=%DATE% %TIME%
-
-REM Add all files to staging
-git add .
-
-REM Commit with the current timestamp as the message
-git commit -m "Auto commit at %CURRENT_TIME%"
-
-REM Push changes to the repository
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set datenow=%%c-%%a-%%b)
+cd D:/myNotes
+git add --all
+git commit -m %%datenow%%
 git push
-
-REM Wait for 20 seconds before the next loop
-timeout /t 20
-
-REM Go back to the start to repeat the process
-goto start
